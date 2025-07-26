c504c0911fd847aee14b76ae2e187fa484b4f23c
Rahul Yadav <rahulydw.dev@gmail.com>
2025-07-26 15:51:32 +0530

    Updated documentation and project screenshot

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className=' absolutew-screen bg-indigo-700 h-[40px] flex justify-center items-center'>
-            <span className='text-white flex justify-center items-center'>&copy; {new Date().getFullYear()} All Rights reserved</span>
+            <span className='text-white flex justify-center gap-1 items-center'>Designed & Developed by <a href="https://linkedin.com/in/iamrahulydw" target="_blank" rel="noopener noreferrer" className='text-white hover:underline'>Rahul Kumar</a> © {new Date().getFullYear()}</span>   
         </div>
     )
 }
